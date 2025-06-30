f87149fb2b8ed0f8d4c69e64d2771d1708963bfa
nivedita647 <nivedita.thakur@esparkbizmail.com>
2025-06-30 13:08:58 +0530

    added db url

--- src/config/dbConnection.ts
+++ src/config/dbConnection.ts
@@ -1,14 +1,33 @@
 import { Sequelize } from "sequelize-typescript";
 import { models } from "../models"
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 dotenv.config();
 
-export const sequelize = new Sequelize({
-    dialect: 'postgres',
-    host:process.env.DB_HOST,
-    username:process.env.DB_USER,
-    password:process.env.DB_PASS,
-    database:process.env.DB_NAME,
-    models,
-    logging:false
-});
+let seq;
+
+if (process.env.DB_URL) {
+    seq = new Sequelize(process.env.DB_URL);
+}
+else{
+    seq = new Sequelize({
+        dialect: 'postgres',
+        host:process.env.DB_HOST,
+        username:process.env.DB_USER,
+        password:process.env.DB_PASS,
+        database:process.env.DB_NAME,
+        models,
+        logging:false
+    })
+}
+
+export const sequelize = seq;
+
+// export const sequelize = new Sequelize({
+//     dialect: 'postgres',
+//     host:process.env.DB_HOST,
+//     username:process.env.DB_USER,
+//     password:process.env.DB_PASS,
+//     database:process.env.DB_NAME,
+//     models,
+//     logging:false
+// });
